51140b0be960410690fb95372daa2c2ca6cc02bf
trietng <93473943+trietng@users.noreply.github.com>
2023-12-16 19:06:51 +0700

    multi-wumpus support, kill_wumpus function, dynamic agent position

--- wumpusworld/agent.py
+++ wumpusworld/agent.py
@@ -1,5 +1,5 @@
 class Agent:
-    def __init__(self, n: int):
-        self.position = (1, 1)
+    def __init__(self, n: int, position: (int, int)):
+        self.position = position
         self.memory = [[set() for _ in range(n)] for _ in range(n)]
         self.score = 0
--- wumpusworld/world.py
+++ wumpusworld/world.py
@@ -1,33 +1,64 @@
 class WumpusWorld:
     def __init__(self, path: str):
-        self.n, self.__world = self.read_map(path)
+        self.n, self.__world, self.agent, self.stenches = self.read_map(path)
 
     @classmethod
-    def set_adjecent_rooms(cls, world, n, i, j):
+    def set_adjecent_rooms(cls, world, n, i, j, stenches):
         signal = 'B' if world[i][j] == 'P' else 'S'
         if i > 0:
-            if signal not in world[i - 1][j]:
-                world[i - 1][j] += signal
+            if signal == 'S':
+                x, y = n - i + 1, j + 1
+                if (x, y) not in stenches:
+                    world[i - 1][j] += signal
+                    stenches.update({(x, y): 1})
+                else:
+                    stenches[(x, y)] += 1
         if i < n - 1:
-            if signal not in world[i + 1][j]:
-                world[i + 1][j] += signal
+            if signal == 'S':
+                x, y = n - i - 1, j + 1
+                if (x, y) not in stenches:
+                    world[i + 1][j] += signal
+                    stenches.update({(x, y): 1})
+                else:
+                    stenches[(x, y)] += 1
         if j > 0:
-            if signal not in world[i][j - 1]:
-                world[i][j - 1] += signal
+            if signal == 'S':
+                x, y = n - i, j
+                if (x, y) not in stenches:
+                    world[i][j - 1] += signal
+                    stenches.update({(x, y): 1})
+                else:
+                    stenches[(x, y)] += 1
         if j < n - 1:
-            if signal not in world[i][j + 1]:
-                world[i][j + 1] += signal
+            if signal == 'S':
+                x, y = n - i, j + 2
+                if (x, y) not in stenches:
+                    world[i][j + 1] += signal
+                    stenches.update({(x, y): 1})
+                else:
+                    stenches[(x, y)] += 1
 
     @classmethod
     def read_map(cls, path: str):
         with open(path, 'r') as f:
             n = int(f.readline())
             world = [[room.replace('-', '') for room in f.readline().strip().replace(' ', '').split('.')] for _ in range(n)]
+        agent = None
+        stenches = {}
         for i in range(n):
             for j in range(n):
-                if world[i][j] == 'W' or world[i][j] == 'P':
-                    cls.set_adjecent_rooms(world, n, i, j)
-        return n, world
+                if world[i][j] == 'P':
+                    cls.set_adjecent_rooms(world, n, i, j, stenches)
+                elif world[i][j] == 'W':
+                    cls.set_adjecent_rooms(world, n, i, j, stenches)
+                    x, y = n - i, j + 1
+                    if (x, y) not in stenches:
+                        stenches.update({(x, y): 1})
+                    else:
+                        stenches[(x, y)] += 1
+                elif world[i][j] == 'A':
+                    agent = n - i, j + 1
+        return n, world, agent, stenches
 
     def row(self, item):
         return self.__world[self.n - item]
@@ -45,3 +76,35 @@ class WumpusWorld:
 
     def __str__(self):
         return f'N = {self.n}\n' + str(self.__world)
+
+    def kill_wumpus(self, position):
+        if 'W' not in self[position]:
+            return False
+        self[position] = self[position].replace('W', '')
+        x, y = position
+        print(self.stenches)
+        if x > 1 and (x - 1, y) in self.stenches:
+            print('nearby', (x - 1, y))
+            self.stenches[(x - 1, y)] -= 1
+            if self.stenches[(x - 1, y)] == 0:
+                self.stenches.pop((x - 1, y))
+                self[(x - 1, y)] = self[(x - 1, y)].replace('S', '')
+        if x < self.n and (x + 1, y) in self.stenches:
+            print('nearby', (x + 1, y))
+            self.stenches[(x + 1, y)] -= 1
+            if self.stenches[(x + 1, y)] == 0:
+                self.stenches.pop((x + 1, y))
+                self[(x + 1, y)] = self[(x + 1, y)].replace('S', '')
+        if y > 1 and (x, y - 1) in self.stenches:
+            print('nearby', (x, y - 1))
+            self.stenches[(x, y - 1)] -= 1
+            if self.stenches[(x, y - 1)] == 0:
+                self.stenches.pop((x, y - 1))
+                self[(x, y - 1)] = self[(x, y - 1)].replace('S', '')
+        if y < self.n and (x, y + 1) in self.stenches:
+            print('nearby', (x, y + 1))
+            self.stenches[(x, y + 1)] -= 1
+            if self.stenches[(x, y + 1)] == 0:
+                self.stenches.pop((x, y + 1))
+                self[(x, y + 1)] = self[(x, y + 1)].replace('S', '')
+        return True
